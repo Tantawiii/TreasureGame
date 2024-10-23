107a955f571ea62ad17957bf3beec00496abca28
Omar Tantawy <o.tantawy.o@gmail.com>
2024-10-23 15:57:41 +0300

    Update treasureGame.cpp

--- treasureGame.cpp
+++ treasureGame.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <conio.h>
+#include <ctime>
+srand(time(0));
+int playerX = rand() % (grid - 2) + 1, playerY = rand() % (grid - 2) + 1;
+char border[][];
+void Movement() {
+    char movement = _getch();
+    int tempX = playerX, tempY = playerY;
+    if (movement == 'w') {
+        tempX--;
+    }
+    else if (movement == 's') {
+        tempX++;
+    }
+    else if (movement == 'a') {
 
+        tempY--;
+    }
+    else if (movement == 'd') {
+
+        tempY++;
+    }
+    Collision(border[][],tempX, tempY);
+}
+void Collision(char border[][],int tempX, int tempY) {
+    if (border[tempX][tempY] != 'H') {
+        border[playerX][playerY] = ' ';
+        playerX = tempX;
+        playerY = tempY;
+        border[playerX][playerY] = 'O';
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
